148c055a2103bea703ef6c78da4eec804e305071
Alakbarbh <alakbarbh@code.edu.az>
2023-03-02 07:21:08 -0800

    add slider carusel

--- Frontend-Project(01.03.2023)/assets/js/home.js
+++ Frontend-Project(01.03.2023)/assets/js/home.js
@@ -37,4 +37,22 @@ $(document).ready(function(){
         let btnText = $(this).html();
         $(".drop1").children().eq(0).html(btnText);
     })
+
+
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 })
