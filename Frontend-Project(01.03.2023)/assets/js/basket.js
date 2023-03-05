d49820c31ed68f6ed6544ab2b09f86bc53b46d25
Alakbarbh <alakbarbh@code.edu.az>
2023-03-05 12:00:52 -0800

    add blog page

---
+++ Frontend-Project(01.03.2023)/assets/js/basket.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $(document).on("click", ".item1", function () {
+        let usdText = $(".item1").children().eq(0).text();
+        $(".drop").children().eq(0).text(usdText)
+    })
+
+    $(document).on("click", ".item2", function () {
+        let eurText = $(".item2").children().eq(0).text();
+        $(".drop").children().eq(0).text(eurText)
+    })
+
+    $(document).on("click", "#down-menu .basketicon", function (e) {
+        e.preventDefault()
+        $(".spCard-dropdown").toggleClass("d-none");
+    });
+
+    
+    $(document).on("click", ".item3", function () {
+        let btnText = $(this).html();
+        $(".drop1").children().eq(0).html(btnText);
+    })
+
+    $(document).on("click", ".item4", function () {
+        let btnText = $(this).html();
+        $(".drop1").children().eq(0).html(btnText);
+    })
+
+    $(document).on("click", ".item5", function () {
+        let btnText = $(this).html();
+        $(".drop1").children().eq(0).html(btnText);
+    })
+
+    $(document).on("click", ".item6", function () {
+        let btnText = $(this).html();
+        $(".drop1").children().eq(0).html(btnText);
+    })
+
+    $(document).on("click", ".item7", function () {
+        let btnText = $(this).html();
+        $(".drop1").children().eq(0).html(btnText);
+    })
+})
